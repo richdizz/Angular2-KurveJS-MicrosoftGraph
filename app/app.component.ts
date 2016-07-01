6be6e0d779dd344d7bd29bc6df8af13b3d857b31
richdizz <richdizz@outlook.com>
2016-06-30 19:05:48 -0500

    added v2 flow

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,23 @@ export class AppComponent {
     private isAuthenticated:boolean = false;
     private messages:kurve.MessageDataModel[];
 
+    //v2 Login
+    login() {
+        const id = new kurve.Identity("ed9162d5-0868-434b-a019-80fdb1add90c", "http://localhost:3000/node_modules/kurvejs/dist/login.html", 
+        {endpointVersion: kurve.EndpointVersion.v2});
+
+        id.loginAsync([kurve.Scopes.Mail.Read]).then(_ => {
+            this.isAuthenticated = true;
+
+            const graph = new kurve.Graph(id);
+            graph.me.messages.GetMessages().then(data => {
+                this.messages = data.value
+            });
+        });
+    }
+
+    /*
+    //v1 Login
     login() {
         const id = new kurve.Identity("1788837a-8e69-4d00-8fb1-a88e0f1ec4b9", "http://localhost:3000/node_modules/kurvejs/dist/login.html", 
         {endpointVersion: kurve.EndpointVersion.v1});
@@ -22,4 +39,5 @@ export class AppComponent {
             });
         });
     }
+    */
 }
